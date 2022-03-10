4e0c647c34457b430fc37897ed1b7b3c3e96ee8f
romass77 <v.romas@hotmail.com>
2022-03-10 22:04:40 +0200

    pirma javascript pamoka

---
+++ index.js
@@ -0,0 +1,29 @@
+//skaiciai
+
+let a=5;
+let b=7;
+let c=9;
+
+let sum1=a*b;
+let sum2=c/b;
+let sum3=a**b;
+let sum4=c-b;
+let liekana=b%a;
+
+let vardas='Jonas';
+let pavarde='jonaitis';
+let gimimoMetai=1980;
+let dabarMetai=2022;
+let atsakymas=`${vardas} ${pavarde} man yra ${dabarMetai-gimimoMetai} metai`
+
+
+
+console.log(atsakymas.length);
+console.log(atsakymas);
+console.log('labas vakaras');
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+console.log(liekana);
+
